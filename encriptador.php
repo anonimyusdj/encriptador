b6fb1b009a388a25a75a18467e0119382d3c152d
anonimyusdj <anonimyusdj@gmail.com>
2018-11-27 14:53:06 -0600

    correpcion de error

--- encriptador.php
+++ encriptador.php
@@ -32,9 +32,10 @@
   }
 
 function descifrar(){
+  //  $cadena=array();
     $cadena=separarPalabras($_REQUEST['palabra']);
-    $producciones=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' '];
-    $alfabeto=['@#','~€¬','1/8','&%','%+5','po','Ew54','?5¿','^ç','78','Tgh','$@@','AxcA','·-:','R_-d','%tYH','UI','PlJ','[;}','{nX}','XxX','=?','&/u','VBm','Io=','Rj45','sp'];
+    $alfabeto=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' '];
+    $producciones=['@#','~€¬','1/8','&%','%+5','po','Ew54','?5¿','^ç','78','Tgh','$@@','AxcA','·-:','R_-d','%tYH','UI','PlJ','[;}','{nX}','XxX','=?','&/u','VBm','Io=','Rj45','sp'];
     $resultado="";
 
     for($i=0;$i < strlen($cadena);$i++){
